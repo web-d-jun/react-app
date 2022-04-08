02713ca4b5535638b006dc0ab958beb040ae7191
cjy87451 <cjy8745@goodksoft.com>
2022-04-08 17:26:12 +0900

    update

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,11 +1,19 @@
 import * as React from "react";
 import Header from "@/layout/header";
 import { Outlet } from "react-router-dom";
+import './layout.scss'
+
+const SubMenu = () => {
+  return(
+    <div className="sub-menu-container">111</div>
+  );
+}
 
 function Layout() {
   return (
     <div>
       <Header test="" />
+      <SubMenu />
       <div className="body">
         <Outlet />
       </div>
