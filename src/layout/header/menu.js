8d6ad294d1e5ae539c077cc7443ac7c8e2e8e5e5
cjy87451 <cjy8745@goodksoft.com>
2022-04-13 11:02:42 +0900

    menu set

--- src/layout/header/menu.js
+++ src/layout/header/menu.js
@@ -36,18 +36,60 @@ const menuItems = [
     name: "제공 Service",
     location: "/service",
     selected: false,
+    subMenu: [
+      {
+        id: 0,
+        name: "분석/지원 서비스",
+        location: "/service/analysis",
+        selected: false,
+      },
+      {
+        id: 1,
+        name: "데이터 컨설팅 서비스",
+        location: "/service/consulting",
+        selected: false,
+      },
+      {
+        id: 2,
+        name: "가명정보 결합 서비스",
+        location: "/service/combination",
+        selected: false,
+      },
+    ],
   },
   {
     id: 2,
     name: "Use Case",
     location: "/use-case",
     selected: false,
+    subMenu: [
+      {
+        id: 0,
+        name: "Reference",
+        location: "/use-case/reference",
+        selected: false,
+      },
+      {
+        id: 1,
+        name: "활용 사례",
+        location: "/use-case/usecase",
+        selected: false,
+      },
+    ],
   },
   {
     id: 3,
     name: "Support",
     location: "/support",
     selected: false,
+    subMenu: [
+      {
+        id: 0,
+        name: "Contact Us",
+        location: "/support/contactus",
+        selected: false,
+      },
+    ],
   },
 ];
 
