caa5bac9c30955fc2c5d2117029a842408f0dfc7
cjy87451 <cjy8745@goodksoft.com>
2022-04-20 11:46:22 +0900

    update

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import imageUrl from "@/assets/img";
 
-import tw from "tailwind-styled-components";
+// import tw from "tailwind-styled-components";
 import styled from "styled-components";
 
-const MainContainerTag = styled.div`
+const MainContainer = styled.div`
   margin-top: 4rem;
   .section-1 {
     width: 100%;
@@ -12,6 +12,9 @@ const MainContainerTag = styled.div`
     margin: 0 auto;
     .box-1 {
       flex: 40%;
+      .box-text {
+        font-size: 3.5rem;
+      }
     }
     .box-2 {
       flex: 30%;
@@ -21,20 +24,18 @@ const MainContainerTag = styled.div`
     }
   }
 `;
-const MainContainer = tw(MainContainerTag)`
-  flex 
-  flex-col
-`;
 
 const MainPage = () => {
   return (
-    <MainContainer>
+    <MainContainer className="flex flex-col">
       <div className="section-1 flex">
         <div className="box-1">
-          <div className="default-font">데이터 구매부터 분석까지</div>
-          <div className="default-font">
-            All you need is
-            <span className="default-font default-color">AccuRator</span>
+          <div className="box-text default-font">데이터 구매부터 분석까지</div>
+          <div className="box-text default-font">
+            All you need is{" "}
+            <span className="box-text default-font default-color">
+              AccuRator
+            </span>
           </div>
         </div>
         <div className="box-2">
