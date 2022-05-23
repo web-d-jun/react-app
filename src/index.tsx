04026751a55114b36fc040ed4b50c1b9d40174ca
cjy87451 <cjy8745@goodksoft.com>
2022-05-23 13:57:47 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,31 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import GraphqlClient from "./graphql-client";
 
 import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
+import { ApolloProvider } from "@apollo/client";
 // eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error, query) => {
-      console.log(error, query, 'global error response......')
+      console.log(error, query, "global error response......");
     },
-    onSuccess: data => {
-      console.log(data,'global success response.........')
-    }
-  })
+    onSuccess: (data) => {
+      console.log(data, "global success response.........");
+    },
+  }),
 });
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ApolloProvider client={GraphqlClient}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/data/index.jsx
+++ src/pages/data/index.jsx
@@ -1,6 +1,35 @@
+import React from 'react'
+
+import { useQuery, gql } from "@apollo/client";
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
 const DataPage = () => {
     return (
-        <div>data...?..</div>
+        <div>data...?..
+            <ExchangeRates />
+        </div>
     )
 };
 export default DataPage;
